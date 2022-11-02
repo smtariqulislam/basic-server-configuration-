e82c87ae9f2a81b4765200e91c55322b2a1ccdc9
S M Tariqul Islam <79934984+smtariqulislam@users.noreply.github.com>
2022-11-02 09:49:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 4000;
 
-
-
-
-
-app.get('/',(req,res)=>{
-    res.send('hello sever')
-})
-
-//middle 
+//middle middleware
 app.use(cors());
+
 app.use(express.json());
 
 
 
+app.get('/',(req,res)=>{
+    res.send('hello sever');
+})
 
 
 app.listen(port,()=>{
