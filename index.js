bc59f0fc1649c80b1b83af8d40010a16a2b32db9
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-11-02 06:44:57 +0600

    basic you need in all project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 4000;
 
@@ -12,36 +12,12 @@ app.get('/',(req,res)=>{
     res.send('hello sever')
 })
 
+//middle 
 app.use(cors());
 app.use(express.json());
 
 
 
-//username: database
-//password: tTnhFzyU3qgknRfz
-
-
-
-const uri = "mongodb+srv://database:tTnhFzyU3qgknRfz@cluster0.781yldu.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-async function run() {
-    try {
-        const database = client.db("insertDB");
-        const haiku = database.collection("haiku");
-        // create a document to insert
-        const doc = {
-            name: "tariqul",
-            hobby: "chess",
-        }
-        const result = await haiku.insertOne(doc);
-
-        console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    } finally {
-        // await client.close();
-    }
-}
-run().catch(console.dir);
 
 
 app.listen(port,()=>{
